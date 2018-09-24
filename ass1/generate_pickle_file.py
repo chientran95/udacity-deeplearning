e58f94f60f040646c6be84620bb0acc5814ba715
chientm <chientm@Chiens-Macbook-Air.local>
2018-09-24 10:37:30 +0700

    Classification + AutoEncoder

--- ass1/generate_pickle_file.py
+++ ass1/generate_pickle_file.py
@@ -1,21 +1,3 @@
-
-# coding: utf-8
-
-# Deep Learning
-# =============
-# 
-# Assignment 1
-# ------------
-# 
-# The objective of this assignment is to learn about simple data curation practices, and familiarize you with some of the data we'll be reusing later.
-# 
-# This notebook uses the [notMNIST](http://yaroslavvb.blogspot.com/2011/09/notmnist-dataset.html) dataset to be used with python experiments. This dataset is designed to look like the classic [MNIST](http://yann.lecun.com/exdb/mnist/) dataset, while looking a little more like real data: it's a harder task, and the data is a lot less 'clean' than MNIST.
-
-# In[3]:
-
-
-# These are all the modules we'll be using later. Make sure you can import them
-# before proceeding further.
 from __future__ import print_function
 import imageio
 import matplotlib.pyplot as plt
@@ -33,9 +15,6 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 # First, we'll download the dataset to our local machine. The data consists of characters rendered in a variety of fonts on a 28x28 image. The labels are limited to 'A' through 'J' (10 classes). The training set has about 500k and the testset 19000 labeled examples. Given these sizes, it should be possible to train models quickly on any machine.
-
-# In[4]:
-
 
 url = 'https://commondatastorage.googleapis.com/books1000/'
 last_percent_reported = None
@@ -80,9 +59,6 @@ test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
 # Extract the dataset from the compressed .tar.gz file.
 # This should give you a set of directories, labeled A through J.
 
-# In[7]:
-
-
 num_classes = 10
 np.random.seed(133)
 
@@ -110,23 +86,16 @@ def maybe_extract(filename, force=False):
 train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
 
-
-# ---
-# Problem 1
 # ---------
 # 
 # Let's take a peek at some of the data to make sure it looks sensible. Each exemplar should be an image of a character A through J rendered in a different font. Display a sample of the images that we just downloaded. Hint: you can use the package IPython.display.
 # 
 # ---
-
 # Now let's load the data in a more manageable format. Since, depending on your computer setup you might not be able to fit it all in memory, we'll load each class into a separate dataset, store them on disk and curate them independently. Later we'll merge them into a single dataset of manageable size.
 # 
 # We'll convert the entire dataset into a 3D array (image index, x, y) of floating point values, normalized to have approximately zero mean and standard deviation ~0.5 to make training easier down the road. 
 # 
 # A few images might not be readable, we'll just skip them.
-
-# In[ ]:
-
 
 image_size = 28  # Pixel width and height.
 pixel_depth = 255.0  # Number of levels per pixel.
@@ -182,17 +151,6 @@ def maybe_pickle(data_folders, min_num_images_per_class, force=False):
 train_datasets = maybe_pickle(train_folders, 45000)
 test_datasets = maybe_pickle(test_folders, 1800)
 
-
-# ---
-# Problem 2
-# ---------
-# 
-# Let's verify that the data still looks good. Displaying a sample of the labels and images from the ndarray. Hint: you can use matplotlib.pyplot.
-# 
-# ---
-
-# ---
-# Problem 3
 # ---------
 # Another check: we expect the data to be balanced across classes. Verify that.
 # 
@@ -201,9 +159,6 @@ test_datasets = maybe_pickle(test_folders, 1800)
 # Merge and prune the training data as needed. Depending on your computer setup, you might not be able to fit it all in memory, and you can tune `train_size` as needed. The labels will be stored into a separate array of integers 0 through 9.
 # 
 # Also create a validation dataset for hyperparameter tuning.
-
-# In[ ]:
-
 
 def make_arrays(nb_rows, img_size):
   if nb_rows:
@@ -263,9 +218,6 @@ print('Testing:', test_dataset.shape, test_labels.shape)
 
 # Next, we'll randomize the data. It's important to have the labels well shuffled for the training and test distributions to match.
 
-# In[ ]:
-
-
 def randomize(dataset, labels):
   permutation = np.random.permutation(labels.shape[0])
   shuffled_dataset = dataset[permutation,:,:]
@@ -275,18 +227,7 @@ train_dataset, train_labels = randomize(train_dataset, train_labels)
 test_dataset, test_labels = randomize(test_dataset, test_labels)
 valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
 
-
-# ---
-# Problem 4
-# ---------
-# Convince yourself that the data is still good after shuffling!
-# 
-# ---
-
 # Finally, let's save the data for later reuse:
-
-# In[ ]:
-
 
 pickle_file = os.path.join(data_root, 'notMNIST.pickle')
 
@@ -306,34 +247,5 @@ except Exception as e:
   print('Unable to save data to', pickle_file, ':', e)
   raise
 
-
-# In[ ]:
-
-
 statinfo = os.stat(pickle_file)
 print('Compressed pickle size:', statinfo.st_size)
-
-
-# ---
-# Problem 5
-# ---------
-# 
-# By construction, this dataset might contain a lot of overlapping samples, including training data that's also contained in the validation and test set! Overlap between training and test can skew the results if you expect to use your model in an environment where there is never an overlap, but are actually ok if you expect to see training samples recur when you use it.
-# Measure how much overlap there is between training, validation and test samples.
-# 
-# Optional questions:
-# - What about near duplicates between datasets? (images that are almost identical)
-# - Create a sanitized validation and test set, and compare your accuracy on those in subsequent assignments.
-# ---
-
-# ---
-# Problem 6
-# ---------
-# 
-# Let's get an idea of what an off-the-shelf classifier can give you on this data. It's always good to check that there is something to learn, and that it's a problem that is not so trivial that a canned solution solves it.
-# 
-# Train a simple model on this data using 50, 100, 1000 and 5000 training samples. Hint: you can use the LogisticRegression model from sklearn.linear_model.
-# 
-# Optional question: train an off-the-shelf model on all the data!
-# 
-# ---
